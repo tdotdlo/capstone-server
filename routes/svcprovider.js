f91b6e53513d15cfd5ca2981a380b4480d178427
Darren Lo <pixelbox.webdev@gmail.com>
2023-05-17 22:43:21 -0400

    Added feature to separate specialists into their each of their own category

--- routes/svcprovider.js
+++ routes/svcprovider.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const svcProviderRouter = express.Router();
 
-const getSvcProvider = require('../utilities.js');
+const { getSvcProvider, specialists } = require('../utilities.js');
 
 svcProviderRouter.route('/services/:id').get((req, res) => {
 	const requestedSvc = req.params.id;
@@ -12,10 +12,36 @@ svcProviderRouter.route('/services/:id').get((req, res) => {
 		(svcProvider) => svcProvider.service === requestedSvc
 	);
 
-	if (req.params.id === 'hairstylist') {
-		res.status(200).send(specificSvcProviders);
-	} else {
-		res.status(404).send('Not available');
+	switch (requestedSvc) {
+		case 'hairstylist':
+			specialists(res, specificSvcProviders);
+			break;
+		case 'barber':
+			specialists(res, specificSvcProviders);
+			break;
+		case 'makeup':
+			specialists(res, specificSvcProviders);
+			break;
+		case 'eyelashes':
+			specialists(res, specificSvcProviders);
+			break;
+		case 'manicure':
+			specialists(res, specificSvcProviders);
+			break;
+		case 'pedicure':
+			specialists(res, specificSvcProviders);
+			break;
+		case 'waxing':
+			specialists(res, specificSvcProviders);
+			break;
+		case 'facial':
+			specialists(res, specificSvcProviders);
+			break;
+		case 'massage':
+			specialists(res, specificSvcProviders);
+			break;
+		default:
+			'Not available';
 	}
 });
 
